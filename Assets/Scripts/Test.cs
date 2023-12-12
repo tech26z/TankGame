71e339c7f9bcf9802da8f11efc7fe11790c1dc7d
fixren <2446821304@qq.com>
2023-12-12 21:37:27 +0800

    ...

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
-    public GameObject Sun;
+    float ho, ve;
+    float speed = 1.0f;
 	// Use this for initialization
 	void Start () {
-		//test;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(Sun.transform);
+        ho = Input.GetAxis("Horizontal");
+        ve = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(ho, 0, ve) * Time.deltaTime * speed;
     }
 }
