3ae4f4c431c89fc570f5820dc118eb342dc0630f
fixren <2446821304@qq.com>
2023-12-13 19:18:03 +0800

    commited 2023-12-13

--- Assets/Scripts/MyTank.cs
+++ Assets/Scripts/MyTank.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class MyTank : MonoBehaviour {
     public float ho, ve;
+    [Header("Shell Prefab")]
+    public GameObject shellPrefab;
+
+    [Header("Move Speed")]
+    public float moveSpeed = 1;
+
+    [Header("Turn Speed")]
+    public float turnSpeed = 1;
+
+    bool fire;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,8 +24,14 @@ public class MyTank : MonoBehaviour {
 	void Update () {
         ho = Input.GetAxis("Horizontal");
         ve = Input.GetAxis("Vertical");
+        fire = Input.GetButtonDown("Fire1");
 
         transform.position += transform.forward * Time.deltaTime * ve;
-        transform.Rotate(0, 1 * ho, 0);
+        transform.eulerAngles += new Vector3(0, ho * turnSpeed, 0);
+        //transform.Rotate(0, 1 * ho, 0);
+        if (fire)
+        {
+            Instantiate(shellPrefab, firePoint.transform.position, identity); //Prefab, position, Quaternion
+        }
 	}
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
     float ho, ve;
-    float speed = 1.0f;
+    public float turnSpeed = 10.0f;
+    int count = 0;
+    float angle;
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("Turned:" + count);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         ho = Input.GetAxis("Horizontal");
-        ve = Input.GetAxis("Vertical");
-
-        transform.position += new Vector3(ho, 0, ve) * Time.deltaTime * speed;
+        
+        transform.eulerAngles += new Vector3(0, Time.deltaTime * ho * turnSpeed, 0);
+        
+        if(ho != 0)
+        {
+            if(ho > 0)
+                angle += transform.rotation.eulerAngles.y;
+            else
+                angle -= transform.rotation.eulerAngles.y;
+            Debug.Log(angle);
+        }
+        
+        
+      
+        //transform.position += new Vector3(ho, 0, ve) * Time.deltaTime * speed;
     }
 }
