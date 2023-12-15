dd09976e338018735c8d91b918e097da2c9e3991
fixren <2446821304@qq.com>
2023-12-15 19:11:44 +0800

    commited 2023-12-15

---
+++ Assets/Scripts/PhysicsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsTest : MonoBehaviour {
+
+    Rigidbody rig;
+
+    private void Awake()
+    {
+        rig = this.GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(rig.velocity);
+        transform.position += Vector3.forward * Time.deltaTime * 3;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.velocity = Vector3.up * 5; //vilocity is force, not transform.position
+        }
+        
+    }
+        
+        
+      
+        
+    
+}
