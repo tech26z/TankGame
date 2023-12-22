ce043a768a7b19077ecf73cf20948d4e3404973f
fixren <2446821304@qq.com>
2023-12-22 20:21:14 +0800

    2023-12-22

--- Assets/EnemyTankControl.cs
+++ Assets/EnemyTankControl.cs
@@ -7,17 +7,28 @@ public class EnemyTankControl : MonoBehaviour {
     public float fireInterval = 3f;
     public float moveSpeed = 3f;
     public float turnSpeed = 1f;
+    public GameObject shellPrefab;
+    public float shellFlySpeed = 30f;
+
     Transform tank;
     float distance;
+    RaycastHit hit;
+    Transform firePoint;
+    float timer;
+
     
 
     private void Awake()
     {
         tank = GameObject.FindWithTag("MyTank").transform;
+        firePoint = transform.Find("Barrel/FirePoint");
     }
 
     // Update is called once per frame
     void Update () {
+
+        timer += Time.deltaTime;
+
        distance = Vector3.Distance(transform.position, tank.position);
         if (distance < 10)
         {
@@ -49,14 +60,44 @@ public class EnemyTankControl : MonoBehaviour {
         transform.rotation = Quaternion.Lerp(transform.rotation, targetQua, Time.deltaTime * turnSpeed);
     }
 
+    //return true if no buddy at front distance
+    bool checkFrontBuddy(float distance)
+    {
+        if(Physics.Raycast(firePoint.position,transform.forward, out hit, distance))
+        {
+            if (hit.collider.transform.root.tag == "EnemyTank")
+                return true;
+        }
+        return false;
+    }
+
     void Fire()
     {
+        TurnToTank();
+        if (checkFrontBuddy(10))
+        {
+            return;
+        }
 
+        if(timer > fireInterval)
+        {
+            GameObject shell = Instantiate(shellPrefab, firePoint.position, Quaternion.identity);
+            Destroy(shell, 3f);
+            //Shell is script, script has both flydir and flySpeed
+            //shell fly direction is forward direction of "this"
+            shell.GetComponent<Shell>().flyDir = transform.forward * shellFlySpeed;
+            timer = 0;
+
+        }
     }
 
     void Aproach()
     {
         TurnToTank();
+        if (!checkFrontBuddy(2))
+        {
+            transform.position += transform.forward * Time.deltaTime * moveSpeed;
+        }
     }
 
     void Roaming()
@@ -64,3 +105,4 @@ public class EnemyTankControl : MonoBehaviour {
 
     }
 }
+
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    float time;
+    int minutes;
+    int seconds;
+
+	// Use this for initialization
+	void Start () {
+        time = 50f;
+        GameObject.Find("timerUI").GetComponent<Text>().text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        seconds = (int)time%60;
+        minutes = (int)time/60;
+        //Debug.Log(minutes + " : " + seconds);
+        GameObject.Find("timerUI").GetComponent<Text>().text = minutes + ":" +
+            seconds;
+	}
+}
